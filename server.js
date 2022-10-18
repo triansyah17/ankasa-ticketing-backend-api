4f401423d3d251920953d7f5586a3a7094647d70
triansyah17 <triansyah.a17@gmail.com>
2022-10-18 19:36:33 +0700

    fix : cors problem

--- server.js
+++ server.js
@@ -2,21 +2,24 @@ const express = require("express");
 const helmet = require("helmet");
 const xss = require("xss-clean");
 const cors = require("cors");
-const { APP_NAME, NODE_ENV, PORT } = require("./src/utils/env");
+const { APP_NAME, NODE_ENV, PORT, CLIENT_URL } = require("./src/utils/env");
 const { failed } = require("./src/utils/createResponse");
 const creditCardRoute = require("./src/router/credit_card.route");
-// const airlinesRoute = require("./src/router/airlines.route");
-// const productRoute = require("./src/router/product.route");
 
 // deklarasi express
 const app = express();
 
 app.use(helmet());
 app.use(xss());
-app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(cors());
+
+const corsOptions = {
+  origin: CLIENT_URL,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 // root router
 app.get("/", (req, res) =>
@@ -24,10 +27,6 @@ app.get("/", (req, res) =>
 );
 // main router
 
-// app.use(airlinesRoute);
-// app.use(productRoute);
-// app.use(require("./src/router/transactions.router"));
-// app.use(require("./src/router/destination.router"));
 app.use(creditCardRoute);
 app.use(require("./src/router/auth.route"));
 app.use(require("./src/router/user.route"));
@@ -42,7 +41,7 @@ app.use("*", (req, res) => {
 });
 
 // running server
-app.listen(process.env.PORT, (req, res) => {
+app.listen(PORT, (req, res) => {
   console.log(`Server started on port ${PORT} with ${NODE_ENV} environment`);
   console.log(`Visit http://localhost:${PORT}`);
   // console.log(req);
--- src/utils/email/sendEmail.js
+++ src/utils/email/sendEmail.js
@@ -1,14 +1,20 @@
-require("dotenv").config();
+const {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  REDIRECT_URI,
+  GMAIL_REFRESH_TOKEN,
+  EMAIL_USER,
+} = require("../env");
 const { google } = require("googleapis");
 const nodemailer = require("nodemailer");
 
 // oauth2 config
 const oAuth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  process.env.REDIRECT_URI
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  REDIRECT_URI
 );
-oAuth2Client.setCredentials({ refresh_token: process.env.GMAIL_REFRESH_TOKEN });
+oAuth2Client.setCredentials({ refresh_token: GMAIL_REFRESH_TOKEN });
 
 const sendEmail = async (dataEmail) => {
   try {
@@ -18,23 +24,13 @@ const sendEmail = async (dataEmail) => {
       service: "gmail",
       auth: {
         type: "OAuth2",
-        user: process.env.EMAIL_USER,
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+        user: EMAIL_USER,
+        clientId: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        refreshToken: GMAIL_REFRESH_TOKEN,
         accessToken,
       },
     });
-
-    // Mailjet SMTP
-    //   host: "in-v3.mailjet.com",
-    //   port: 2525,
-    //   secure: false,
-    //   auth: {
-    //     user: "00849e2b331d96466ab13943820c30fc",
-    //     pass: "20051afefbb4535d70686171387a499a",
-    //   },
-    // });
 
     // send email
     transporter
